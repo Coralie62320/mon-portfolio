f5d8c595b28934a19c20e9ed97af69c4756ae3e7
lougezcoralie@hotmail.fr <lougezcoralie@hotmail.fr>
2023-11-21 08:34:26 +0100

    Mise à jour

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-let navBar = document.querySelector("nav");
-let icon = document.querySelector("i");
-
-
-function openMenu() {
-    let nav = document.querySelector("nav")
-    nav.classList.toggle("open")
-}
-function changeIcon(icon) {
-    icon.classList.toggle("fa-times")
-}
+// function openMenu() {
+//     let nav = document.querySelector("nav")
+//     nav.classList.toggle("open")
+// }
+// function changeIcon(icon) {
+//     icon.classList.toggle("fa-times")
+// }
 
 
 
-// let hamMenuIcon = document.querySelector("#ham-menu");
-// let navBar = document.querySelector("#nav-bar");
-// let navLinks = navBar.querySelectorAll("li");
-// hamMenuIcon.addEventListener("click", () => {
-//     navBar.classList.toggle("active");
-//     hamMenuIcon.classList.toggle("fa-times");
-// });
-// navLinks.forEach((navLinks) => {
-//     navLinks.addEventListener("click", () => {
-//         navBar.classList.remove("active");
-//         hamMenuIcon.classList.toggle("fa-times");
-//     });
-// });
+let hamMenuIcon = document.querySelector("#ham-menu");
+let navBar = document.querySelector("#nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+hamMenuIcon.addEventListener("click", () => {
+    navBar.classList.toggle("active");
+    hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+    navLinks.addEventListener("click", () => {
+        navBar.classList.remove("active");
+        hamMenuIcon.classList.toggle("fa-times");
+    });
+});
+
 
 
 // document.addEventListener("scroll", function() {
