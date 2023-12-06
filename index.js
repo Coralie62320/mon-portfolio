1539dc3d99d2b000750ba6c39328cc33fba15ef2
lougezcoralie@hotmail.fr <lougezcoralie@hotmail.fr>
2023-12-06 16:09:34 +0100

    Mise à jour

--- index.js
+++ index.js
@@ -28,6 +28,39 @@ navLinks.forEach((navLinks) => {
 });
 
 
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+    
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    };
+};
+
+window.addEventListener("scroll", reveal);
+
+function revealDeux() {
+    let revealsDeux = document.querySelectorAll(".reveal-2");
+    for (let i = 0; i < revealsDeux.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = revealsDeux[i].getBoundingClientRect().top;
+        let elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+          revealsDeux[i].classList.add("active");
+        } else {
+          revealsDeux[i].classList.remove("active");
+        }
+      };
+};
+
+window.addEventListener("scroll", revealDeux);
+
 
 
 
